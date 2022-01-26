eba9a4b42d60955ba85952789426d3b9b27b5bb5
Abbas Dargahi <adargahi97@gmail.com>
2022-01-26 11:32:44 -0600

    Assignment 3.1

--- payCalculator.py
+++ payCalculator.py
@@ -5,12 +5,15 @@ def calculatePay():
     rate = input("Enter Rate: ")
     overtimeHrs = 0
     overtimeRate = float(rate) * 1.5
+    gross_pay = 0
     
     if float(hrs) > 40:
         overtimeHrs = float(hrs)-40
         hrs = 40
-    
-    gross_pay = (float(hrs) * float(rate)) + (float(overtimeHrs) * float(overtimeRate))
+        gross_pay = (float(hrs) * float(rate)) + (float(overtimeHrs) * float(overtimeRate))
+    else:
+        gross_pay = float(hrs) * float(rate)
+
     print(gross_pay)
     
     
@@ -20,4 +23,4 @@ def calculatePay():
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-#calculatePay()
+calculatePay()
